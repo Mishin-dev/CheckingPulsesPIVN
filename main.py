67c8cb9033607845882e9ed345d1ed1832fc96f6
Mishin-dev <batman.bag@yandex.ru>
2025-02-17 23:59:20 +0300

    First commit. Added functions to read .csv files.

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
-import csv
-import 
+import pandas as pd
+from matplotlib import pyplot as plt
+from numpy.ma.extras import unique
+from pandas.core.algorithms import nunique_ints
+from pytz import unicode
+
+# df = pd.read_csv('data/03-06-2024 11-41-40.csv', delimiter=';')
+# df = pd.read_csv('data/09-10-2024 15-06-33.csv', delimiter=';')
+# df = pd.read_csv('data/09-10-2024 15-19-18.csv', delimiter=';')
+# df = pd.read_csv('data/09-10-2024 15-57-05.csv', delimiter=';')
+df = pd.read_csv('data/27-05-2024 16-38-07.csv', delimiter=';')
+print(df)
+plt.plot(df['t'], df['sum'])
+# plt.show()
+
+print(nunique_ints(df['tr']))
 
 
 
